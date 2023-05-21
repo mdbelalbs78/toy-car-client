d8b447d61100a56374e54b7ff6a094c994b12419
belalhossainS <mdbelal01792@gmail.com>
2023-05-21 13:25:12 +0600

    private route

--- src/Routes/PrivateRoute.jsx
+++ src/Routes/PrivateRoute.jsx
@@ -6,7 +6,7 @@ import { AuthContext } from '../providers/AuthProviders';
 const PrivateRoute = ({children}) => {
     const {user,loading} = useContext(AuthContext)
     const location = useLocation();
-    console.log('urrr route', user);
+    console.log('your route', user);
     
 if(loading){
     return <div>
